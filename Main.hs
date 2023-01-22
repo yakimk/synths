f4195f3d780fb4837d78aae986cbe5bcb4dfb016
yakimk <yakimenko.kir@gmail.com>
2023-01-23 00:31:22 +0100

    Added play function and controls for sample rate

--- Main.hs
+++ Main.hs
@@ -1,21 +1,34 @@
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as Bl
 import Data.Foldable ()
+import System.Process ( runCommand )
+import Text.Printf ( printf )
 
 type Wave = [Float]
 
 volume :: Float
 volume = 0.5
 
+outPath :: FilePath
+outPath = "output.bin"
+
+sampleRate :: Float
+sampleRate = 48000
 
 step :: Float
-step = 0.01
+step = 0.03
 
 soundPrep :: Wave -> Wave
 soundPrep = map ((* volume) . sin . (* step)) 
 
 wave :: [Float]
-wave = soundPrep [0.0 .. 48000]
+wave = soundPrep [0.0 .. sampleRate]
 
-save :: IO()    
-save = B.writeFile "output.bin" $ Bl.toLazyByteString $ foldMap Bl.floatLE wave 
+save :: FilePath ->  IO()    
+save filePath = B.writeFile filePath $ Bl.toLazyByteString $ foldMap Bl.floatLE wave 
+
+play :: IO()
+play = do
+    save outPath
+    _ <- runCommand $ printf "ffplay -f f32le -ar  %f %s" sampleRate outPath
+    return ()
