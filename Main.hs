35005aaef9721e6ef45f4531037fcb302ea42091
yakimk <yakimenko.kir@gmail.com>
2023-01-23 00:14:39 +0100

    What is this shit?

--- Main.hs
+++ Main.hs
@@ -1,3 +1,7 @@
+import qualified Data.ByteString as B
+import Data.ByteString.Builder as B ( toLazyByteString, floatLE )
+import Data.Foldable ()
+
 volume :: Float
 volume = 0.5
 
@@ -7,7 +11,10 @@ step :: Float
 step = 0.01
 
 soundPrep :: Wave -> Wave
-soundPrep = map (sin . (* step)) 
+soundPrep = map ((* volume) . sin . (* step)) 
 
 wave :: [Float]
 wave = soundPrep [0.0 .. 48000]
+
+save :: IO()    
+save = B.writeFile "output.bin" $ B.toLazyByteString $ foldMap B.floatLE wave 
